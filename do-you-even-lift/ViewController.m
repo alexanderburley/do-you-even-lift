078ec13c69f393a48cb378e4adca3a621eea4e34
alexburley3@gmail.com <aca13ab@f19.local>
2017-01-07 17:12:45 +0000

    Initial Commit

---
+++ do-you-even-lift/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  do-you-even-lift
+//
+//  Created by Alex Burley on 07/01/2017.
+//  Copyright © 2017 Alex Burley. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
